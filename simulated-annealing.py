989e2fecc0fcc2caf7de11f4aa9b1fe92534909a
Vincenzo Bucaria <87119887+vincenzobucaria@users.noreply.github.com>
2024-04-10 16:35:20 +0200

    Add files via upload

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -9,7 +9,12 @@ Università degli Studi di Messina
 
 work in progress
 
-last update 08/04/2024
+last update 10/04/2024
+
+
+- aggiunto il sistema a punti anche per il collegamento tra power nodes
+- risolto il problema per cui potevano essere generati dei loop o avere soluzioni con grafi non connessi
+- sistemati i parametri per il sistema a punti dei nodi constrained
 
 """
 
@@ -88,95 +93,189 @@ def draw_graph(G):
     plt.show()
 
 
+
+
+
+
+
 def generate_greedy_initial_solution(graph, constrained_nodes, power_nodes):
     
     initial_solution_graph = nx.Graph()
 
+    def connect_constrained_nodes():
 
 
-    #Mediante la relazione utilizzata si tenta di cercare un compromesso tra
     
-    # La miglior latenza tra il nodo constrained e il suo nodo power afferente
-    # Il maggior numero di altri nodi constrained contattabili direttamente per mezzo del power node
-    # Latenza media dei link gestiti dal power node 
-    
-    # In questo modo è possibile minimizzare il numero di hop
-
-
-    for constrained_node in constrained_nodes:
+        for constrained_node in constrained_nodes:
         
-        minimum_latency = 10000 #as like as infinite
-        best_power_node_score = 0
+            minimum_latency = 10000 #as like as infinite
+            best_power_node_score = 0
 
-        for power_node in power_nodes:
+            for power_node in power_nodes:
 
-            edge_latency = graph[constrained_node][power_node]["latency"]
+                edge_latency = graph[constrained_node][power_node]["latency"]
+                
+                
+                if power_node in initial_solution_graph.nodes(): #Enables advanced score  
             
-            
-            if power_node in initial_solution_graph.nodes(): #Enables advanced score  
-           
-                constrained_neighbours = list(initial_solution_graph.neighbors(power_node))
-                number_of_constrained_neighbours = len(constrained_neighbours)
-                
-                for constrained in constrained_neighbours:
-                    neighbours_latency = initial_solution_graph[constrained][power_node]["latency"]
-                neighbours_average_latency = neighbours_latency/number_of_constrained_neighbours
-                
-                if debug:
-                    print("Neighbours of", power_node, "are:", constrained_neighbours, "Average latency: ", neighbours_average_latency, "ms")
-            else:
-                
-                number_of_constrained_neighbours = 1
-                neighbours_average_latency = 0
-                        
-            power_node_score = (1.4*number_of_constrained_neighbours)/(edge_latency+0.5*neighbours_average_latency)   
-                
-            if(power_node_score>best_power_node_score):
-                best_power_node_score=power_node_score
-                best_power_node = power_node
-                minimum_latency = edge_latency
-                type_of_power_node = graph.nodes[best_power_node]["node_type"]
+                    constrained_neighbours = list(initial_solution_graph.neighbors(power_node))
+                    number_of_constrained_neighbours = len(constrained_neighbours)
                     
+                    for constrained in constrained_neighbours:
+                        neighbours_latency = initial_solution_graph[constrained][power_node]["latency"]
+                    neighbours_average_latency = neighbours_latency/number_of_constrained_neighbours
+                    
+                    if debug:
+                        print("Neighbours of", power_node, "are:", constrained_neighbours, "Average latency: ", neighbours_average_latency, "ms")
+                else:
+                    
+                    number_of_constrained_neighbours = 1
+                    neighbours_average_latency = 0
+                            
+                #Come posso calcolare il punteggio? 
+
+                #Più è bassa la latenza diretta tra il nodo constrained e il power node e più alto sarà il punteggio
+                
+                direct_latency_score = 60/(edge_latency)
+
+                #Se ci sono già altri nodi constrained (nel limite della capacity) il punteggio può aumentare perchè possiamo diminuire il numero di hop
+
+                neighbours_quantity_score = 0.2*number_of_constrained_neighbours
+
+                #Se la latenza dei nodi constrained affenti al power node è bassa il punteggio può aumentare
+                if number_of_constrained_neighbours>1:
+                    neighbours_latency_score = 10/(neighbours_average_latency)
+                else:
+                    neighbours_latency_score = 0
+                
                 
 
-        print("the best power node for node", constrained_node, "is the",type_of_power_node, "node", best_power_node, "which minimum latency is", minimum_latency, "ms")
-        
-        
-        initial_solution_graph.add_node(constrained_node, node_type="constrained")
-        initial_solution_graph.add_node(best_power_node, node_type=type_of_power_node)
-        initial_solution_graph.add_edge(constrained_node, best_power_node, latency=minimum_latency)
-        
-    for first_power_node in power_nodes:
-        
-        #Questo non funziona in quanto non abbiamo nessuna garanzia che il grafo sia connesso!
-        # ci devo lavorare
-        
-        
-        minimum_latency = 10000 #as like as infinite
-        type_of_first_power_node = graph.nodes[first_power_node]["node_type"]
-        for second_power_node in power_nodes:
-            if second_power_node != first_power_node:
-                edge_latency = graph[first_power_node][second_power_node]["latency"]
-            
-                if edge_latency < minimum_latency:
-                    best_power_node = second_power_node
-                    type_of_second_power_node = graph.nodes[best_power_node]["node_type"]
+                
+                power_node_score = direct_latency_score+neighbours_quantity_score+neighbours_latency_score
+
+
+                ''
+                print("Score for power node", power_node, "is: ", power_node_score, "direct latency: ", edge_latency, "average neighbours latency: ", neighbours_average_latency) 
+                    
+                if(power_node_score>best_power_node_score):
+                    best_power_node_score=power_node_score
+                    best_power_node = power_node
                     minimum_latency = edge_latency
+                    type_of_power_node = graph.nodes[best_power_node]["node_type"]
+                        
                     
-                
-                
-        initial_solution_graph.add_node(first_power_node, node_type=type_of_first_power_node)
-        initial_solution_graph.add_node(best_power_node, node_type=type_of_second_power_node)
-        initial_solution_graph.add_edge(first_power_node, best_power_node, latency=minimum_latency)
+
+            print("the best power node for node", constrained_node, "is the",type_of_power_node, "node", best_power_node, "which minimum latency is", minimum_latency, "ms")
             
+            
+            initial_solution_graph.add_node(constrained_node, node_type="constrained")
+            initial_solution_graph.add_node(best_power_node, node_type=type_of_power_node)
+            initial_solution_graph.add_edge(constrained_node, best_power_node, latency=minimum_latency)
+
+        return initial_solution_graph
+            
+        #Mediante la relazione utilizzata si tenta di cercare un compromesso tra
         
+        # La miglior latenza tra il nodo constrained e il suo nodo power afferente
+        # Il maggior numero di altri nodi constrained contattabili direttamente per mezzo del power node
+        # Latenza media dei link gestiti dal power node 
+        
+        # In questo modo è possibile minimizzare il numero di hop
+
+
+    def connect_power_nodes():   
+
+    
+        def sort_power_nodes():
+
+            power_nodes_sorted_list = list()
+
+            for power_node in power_nodes:
+                
+                if power_node in initial_solution_graph.nodes():
+                    power_nodes_sorted_list.append((power_node, len(initial_solution_graph.adj[power_node])))
+                else:
+                    power_nodes_sorted_list.append((power_node, 0))
+        
+
+            power_nodes_sorted_list = data_ordinata = sorted(power_nodes_sorted_list, key=lambda x: x[1], reverse=True)
+
+           
+            return power_nodes_sorted_list
+        
+        sorted_power_nodes_tuples = sort_power_nodes()
+
+
+        processed_power_nodes = list()
+
+        for first_power_node_tuple in sorted_power_nodes_tuples:
+            
+            first_power_node = first_power_node_tuple[0]
+            
+            #Questo non funziona in quanto non abbiamo nessuna garanzia che il grafo sia connesso!
+            # ci devo lavorare
+            
+
+            minimum_latency = 10000 #as like as infinite
+            type_of_first_power_node = graph.nodes[first_power_node]["node_type"]
+
+            
+            add_to_graph = 0
+            best_power_score = 0
+            for second_power_node_tuple in sorted_power_nodes_tuples:
+                second_power_node = second_power_node_tuple[0]
+                if second_power_node != first_power_node:
+                    edge_latency = graph[first_power_node][second_power_node]["latency"]
+                
+                    
+                    if (second_power_node not in processed_power_nodes):
+
+                        
+                        second_power_node_quantity_score = second_power_node_tuple[1]
+
+                        second_power_node_latency_score = 100/edge_latency
+
+                        second_power_score = second_power_node_latency_score + second_power_node_quantity_score
+                        
+
+                        print("debug: Score for power", second_power_node, "is: ", second_power_score, ". Quantity score:", second_power_node_quantity_score, "Latency score: ", second_power_node_latency_score)
+                        
+                        if second_power_score > best_power_score:
+                            best_power_node = second_power_node
+                            type_of_second_power_node = graph.nodes[best_power_node]["node_type"]
+                            best_power_score = second_power_score
+                            best_power_score_latency = edge_latency
+                            add_to_graph = 1
+            processed_power_nodes.append(first_power_node)
+            print("best node ", best_power_node)        
+            if add_to_graph:
+                initial_solution_graph.add_node(first_power_node, node_type=type_of_first_power_node)
+                initial_solution_graph.add_node(best_power_node, node_type=type_of_second_power_node)
+                initial_solution_graph.add_edge(first_power_node, best_power_node, latency=best_power_score_latency)
+        
+        
+        
+        print("fatto")
     
     
-        
-        
-  
-        
+    
+    initial_solution_graph = connect_constrained_nodes()
+    connect_power_nodes()
+
+
+
+            
     return initial_solution_graph
+
+
+
+
+def is_graph_connected():
+    
+    #restituisce true se il grafo è connesso, viceversa false
+    
+    pass
+
 
 
 
@@ -202,9 +301,9 @@ max_iterations_number = 100 #to be defined ...
 # _____________________ editable network parameters  ____________________________
 
 
-total_nodes_quantity = 10                                            #Total quantity of nodes composing the graph. At the moment for test purpose let this quantity be 10
-constrained_nodes_quantity = int(0.7*total_nodes_quantity)              #Total quantity of constrained nodes, all of them must be connected in the graph with just 1 arc for node.
-mandatory_power_nodes_quantity = int(0.2*(total_nodes_quantity))        #Quantity of mandatory nodes: that is, they must be included in the graph and each of them can have more then 1 arc.
+total_nodes_quantity = 20  #Total quantity of nodes composing the graph. At the moment for test purpose let this quantity be 10
+constrained_nodes_quantity = int(0.7*total_nodes_quantity)      #Total quantity of constrained nodes, all of them must be connected in the graph with just 1 arc for node.
+mandatory_power_nodes_quantity = int(0.2*(total_nodes_quantity))   #Quantity of mandatory nodes: that is, they must be included in the graph and each of them can have more then 1 arc.
 discretionary_power_nodes_quantity = total_nodes_quantity-constrained_nodes_quantity-mandatory_power_nodes_quantity #Quantity of power nodes that can be used for getting a better spanning tree but are not mandatory
 
 
